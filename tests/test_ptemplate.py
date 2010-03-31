a8e3b0c8532cb72f3cbd003abb7f718d5b15d175
Will Maier <willmaier@ml1.net>
2010-03-31 15:55:57 -0500

    Start adding tests (they pass).

---
+++ tests/test_ptemplate.py
@@ -0,0 +1,33 @@
+from tests import BaseTest
+
+from ptemplate import PFormatter
+
+class TestPFormatter(BaseTest):
+
+    def setUp(self):
+        self.formatter = PFormatter()
+        self.data = {
+            "foo": "the foo",
+            "results": [
+                {"thisis": "letters", "one": "a", "two": "b"},
+                {"thisis": "digits", "one": "1", "two": "2"},
+            ]
+        }
+        self.input = """\
+{% this is a comment}
+la la la
+{/dne}
+{#results}
+this is: {thisis}
+this is from a different scope: {foo}
+
+one: {one}
+two: {two}
+and some more text
+{/results}
+and something after the section
+"""
+
+    def test_standard_formatter(self):
+        out = self.formatter.format("foo")
+        self.assertEqual(out, "foo")
