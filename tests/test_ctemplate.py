cda10b73172348d740d8ea88adf8461148c3a5e0
Will Maier <willmaier@ml1.net>
2010-04-01 14:52:50 -0500

    Start porting google tests.

--- tests/test_ctemplate.py
+++ tests/test_ctemplate.py
@@ -14,3 +14,23 @@ class TestCTemplate(BaseTest):
         data = {"foo": "bar"}
         input = "{{foo}}"
         self.assertEqual("bar", self.format(input, **data))
+
+    # The following are ported from Google's template_unittest.cc,
+    # starting around line 400.
+    def test_weird_syntax(self):
+        data = {}
+        input = "hi {{{! VAR {{!VAR} }} lo"
+        # XXX
+        #self.assertEqual("hi { lo", self.format(input, **data))
+
+        input = "fn(){{{BI_NEWLINE}} x=4;{{BI_NEWLINE}}}"
+        self.assertEqual("fn(){\n x=4;\n}", self.format(input, **data))
+
+        input = "{{{{{{VAR}}}}}}}}"
+        # XXX
+        #self.assertEqual("{{{{}}}}}}", self.format(input, **data))
+
+    def test_comment(self):
+        data = {}
+        input = "hi {{!VAR}} lo"
+        self.assertEqual("hi  lo", self.format(input, **data))
