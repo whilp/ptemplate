2dbfa4c70ba014e22a7581dbf82fd1a0aee821da
Will Maier <willmaier@ml1.net>
2010-04-01 14:52:41 -0500

    Add support for globals.

--- lib/ptemplate/ctemplate.py
+++ lib/ptemplate/ctemplate.py
@@ -3,6 +3,10 @@ from ptemplate.template import Template
 __all__ = ["CTemplate"]
 
 class CTemplate(Template):
+    globals = {
+        "BI_NEWLINE": '\n',
+        "BI_SPACE": ' ',
+    }
 
     def preprocessor(self, input):
         input = input.replace("{", "{{")
@@ -10,4 +14,11 @@ class CTemplate(Template):
         input = input.replace("}", "}}")
         input = input.replace("}}}}", "}")
 
+        input = input.replace("{!", "{%")
+
         return input
+
+    def _vformat(self, format_string, args, kwargs, used, depth):
+		globals = self.globals.copy()
+		globals.update(kwargs)
+		return super(CTemplate, self)._vformat(format_string, args, globals, used, depth)
