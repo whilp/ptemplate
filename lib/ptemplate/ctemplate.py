e551f2b66da7abe927b18a89cf6184ed63bb5bd2
Will Maier <willmaier@ml1.net>
2010-04-08 02:40:26 -0500

    Some docs.

--- lib/ptemplate/ctemplate.py
+++ lib/ptemplate/ctemplate.py
@@ -1,4 +1,27 @@
-"""
+"""\
+:mod:`ptemplate.ctemplate` -- ctemplate-like interface
+------------------------------------------------------
+
+This module implements a templating interface similar Google's _`ctemplate
+<http://google-ctemplate.googlecode.com/>` with a few important exceptions:
+
+* templates may not change the field delimiter
+* modifiers are marked with '!'
+* comments are marked with '%'
+* the templater does not strip whitespace (except by modifiers)
+* includes are not supported
+* pragmas/macros are not supported
+
+
+
+Like ctemplate, :class:`CTemplate` expands a string template to match the
+structure of a dictionary.k
+Data dictionaries may contain either lists of other
+data dictionaries or single values (string, integer, float, etc). Plain
+variables are substituted according to the usual Python string formatting rules.
+Sections (fields preceded by '#') are expanded once for each data dictionary
+contained in the corresponding list.
+
 Known differences from Google's ctemplate:
     * no modifiers
     * no whitespace stripping
