601f123b7fb1f2c6679615fef4dcacee2160642d
Will Maier <willmaier@ml1.net>
2010-04-14 08:46:55 -0500

    Make sphinx conf work.

--- docs/conf.py
+++ docs/conf.py
@@ -13,7 +13,7 @@
 
 import sys, os
 
-import %PROJECT% as pkg
+import ptemplate as pkg
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
